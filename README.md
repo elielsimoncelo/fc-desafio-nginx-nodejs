ce69b80dec4124abeb01424026e6e255d5841310
Eliel Simoncelo de Sousa <sousaeliel@gmail.com>
2022-05-28 19:29:34 -0300

    chore: create project

---
+++ README.md
@@ -0,0 +1,17 @@
+# nginx com nodejs
+
+## Desafio
+
+- Criar um nginx como proxy reverso para o nodejs
+- Ao acessar a pagina incial o usuario pode visualizar o nomes das pessoas cadastrados no banco de dados
+- O sistema deve permitir cadastrar uma nova pessoa no banco de dados
+
+## Como executar
+
+```sh
+    docker-compose up -d --build
+```
+
+## Como acessar
+
+[http://localhost:8080](http://localhost:8080)
---
+++ app/routes/index.js
@@ -0,0 +1,42 @@
+#!/usr/bin/env node
+
+const express = require('express');
+const router = express.Router();
+const createError = require('http-errors');
+const PeopleDbConnection = require('../data/people-dbconnection');
+
+router.get('/', function(req, res, next) {
+  const db = PeopleDbConnection();
+
+  try {
+    db.query('select * from people', (err, rows) => { 
+      if (err) {
+        throw err;
+      }
+
+      const result = JSON.parse(JSON.stringify(rows));
+      
+      res.render('index', { title: 'Full Cycle Rocks!', people: result });
+    });
+  } catch (error) {
+    next(createError(500, new Error('Não foi possivel buscar as pessoas cadastradas.')));
+  } finally {
+    db.end();
+  }
+});
+
+router.post('/', function(req, res, next) {
+  const db = PeopleDbConnection();
+
+  try {
+    db.query('insert into people (name) values (?)', [req.body.name]);
+
+    res.redirect('/');
+  } catch (error) {
+    next(createError(500, new Error('Não foi possivel inserir a pessoa.')));
+  } finally {
+    db.end();
+  }
+});
+
+module.exports = router;
